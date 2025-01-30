094982b97a57f4f8a41d223e6cb73c6d4471a7c1
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-01-30 02:01:31 +0100

    Add lualine theme

---
+++ lua/lualine/themes/b16.lua
@@ -0,0 +1,51 @@
+local c = require("b16").cols
+return {
+	normal = {
+		a = { fg = c.base00, bg = c.base0D, gui = "bold" },
+		b = { fg = c.base05, bg = "NONE" },
+		c = { fg = c.base05, bg = "NONE" },
+		x = { fg = c.base05, bg = "NONE" },
+		y = { fg = c.base05, bg = "NONE" },
+		z = { fg = c.base00, bg = c.base0D, gui = "bold" },
+	},
+	insert = {
+		a = { fg = c.base00, bg = c.base0B, gui = "bold" },
+		b = { fg = c.base05, bg = "NONE" },
+		c = { fg = c.base05, bg = "NONE" },
+		x = { fg = c.base05, bg = "NONE" },
+		y = { fg = c.base05, bg = "NONE" },
+		z = { fg = c.base00, bg = c.base0B, gui = "bold" },
+	},
+	visual = {
+		a = { fg = c.base00, bg = c.base0E, gui = "bold" },
+		b = { fg = c.base05, bg = "NONE" },
+		c = { fg = c.base05, bg = "NONE" },
+		x = { fg = c.base05, bg = "NONE" },
+		y = { fg = c.base05, bg = "NONE" },
+		z = { fg = c.base00, bg = c.base0E, gui = "bold" },
+	},
+	replace = {
+		a = { fg = c.base00, bg = c.base09, gui = "bold" },
+		b = { fg = c.base05, bg = "NONE" },
+		c = { fg = c.base05, bg = "NONE" },
+		x = { fg = c.base05, bg = "NONE" },
+		y = { fg = c.base05, bg = "NONE" },
+		z = { fg = c.base00, bg = c.base09, gui = "bold" },
+	},
+	command = {
+		a = { fg = c.base00, bg = c.base08, gui = "bold" },
+		b = { fg = c.base05, bg = "NONE" },
+		c = { fg = c.base05, bg = "NONE" },
+		x = { fg = c.base05, bg = "NONE" },
+		y = { fg = c.base05, bg = "NONE" },
+		z = { fg = c.base00, bg = c.base08, gui = "bold" },
+	},
+	inactive = {
+		a = { fg = c.base00, bg = c.base03, gui = "bold" },
+		b = { fg = c.base05, bg = "NONE" },
+		c = { fg = c.base05, bg = "NONE" },
+		x = { fg = c.base05, bg = "NONE" },
+		y = { fg = c.base05, bg = "NONE" },
+		z = { fg = c.base00, bg = c.base03, gui = "bold" },
+	},
+}
